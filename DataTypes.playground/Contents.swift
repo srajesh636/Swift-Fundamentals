5ff517b06bc60a8a788d4ea6403c6e1552fe5da1
U Sai Rajesh <srajesh636@gmail.com>
2020-04-04 02:53:52 +0530

    chore(swift) : initial commit

---
+++ DataTypes.playground/Contents.swift
@@ -0,0 +1,47 @@
+import UIKit
+
+// Basic Data types in swift are : (int,float,double,bool,string)
+
+// Declaration of variables have to be given with type
+
+var Int: Int
+var String: String
+var Float: Float
+var Boolean: Bool
+var Double: Double
+
+// What is difference between float and double
+// float is 32bit
+// Double is 64bit
+
+var string = "Hello"
+var int = 20
+var boolean = false
+var float = 3.8
+var double = 12.3453255
+
+// Constants in Swift is denoted by let
+
+let a=10
+
+// nill is equal to a null
+//optional means you don't its value now but you know its type and value may come or may not
+var result:String? = nil
+
+
+// Array here in swift array is a collection of similar data types.
+
+var range: [Int]
+range=[1,2,3]
+
+// To get data types types :
+
+type(of: range)
+
+
+// Optionals are basically you tell that you are expecting some value in future and u know its type so it can come or may not come
+
+var total:Int?
+
+total = 10
+total!
